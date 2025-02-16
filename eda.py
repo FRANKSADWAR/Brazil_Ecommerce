bb49269250902a9323bd71b71f13a4f856d62980
FRANKSADWAR <billyfranks98@gmail.com>
2025-02-16 17:09:36 +0300

    group and sort

--- eda.py
+++ eda.py
@@ -379,7 +379,7 @@ ax3.set_title('Sum of freight value all over Brazil')
 
 ## What about delivery times in the states ? Let's find out
 purchasing = pd.to_datetime(df_orders_filt['order_purchase_timestamp'])
-delivered = pd.ti_datetime(df_orders_filt['order_delivered_customer_date'])
+delivered = pd.to_datetime(df_orders_filt['order_delivered_customer_date'])
 estimated = pd.to_datetime(df_orders_filt['order_estimated_delivery_date'])
 
 df_orders_filt['time_to_delivery'] =  (delivered - purchasing).dt.days
@@ -387,6 +387,5 @@ df_orders_filt['diff_estimated_delivery'] = (delivered - estimated).dt.days
 
 states_avg_grouped = df_orders_filt.groupby(by='customer_state', as_index=False)['freight_value'].mean()
 states_freight_paid = states_avg_grouped.loc[:, ['customer_state','freight_value']].sort_values(by='freight_value', ascending = False)
-states_time_to_delivery = df_orders_filt.groupby(by='customer_state')['time_to_delivery'].mean().sort_values(by='time_to_delivery', ascending=False)
-states_estimated_delivery = df_orders_filt.groupby(by='customer_state')['diff_estimated_delivery'].mean().sort_values(by='diff_estimated_delivery', ascending = False)
-
+states_time_to_delivery = df_orders_filt.groupby(by='customer_state', as_index=False)['time_to_delivery'].mean().sort_values(by='time_to_delivery', ascending=False)
+states_estimated_delivery = df_orders_filt.groupby(by='customer_state', as_index=False)['diff_estimated_delivery'].mean().sort_values(by='diff_estimated_delivery', ascending=False)
