eab86bee76a71bcf6eaaa61b1b2a30114fd16a5d
FRANKSADWAR <billyfranks98@gmail.com>
2025-02-19 21:47:30 +0300

    cleaning text

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,4 +8,3 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.base import BaseEstimator, TransformerMixin
 
-count = CountVectorizer()
