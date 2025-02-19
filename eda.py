56e4cf4b8e74250995a92ee34847f6af0376688b
FRANKSADWAR <billyfranks98@gmail.com>
2025-02-19 21:51:25 +0300

    reviews dataframe

--- eda.py
+++ eda.py
@@ -21,7 +21,6 @@ olist_geolocation = pd.read_csv(path +'olist_geolocation_dataset.csv')
 olist_orders = pd.read_csv(path +'olist_orders_dataset.csv')
 olist_order_items = pd.read_csv(path+'olist_order_items_dataset.csv')
 olist_order_payments = pd.read_csv(path+'olist_order_payments_dataset.csv')
-olist_order_reviews = pd.read_csv(path+'olist_order_reviews_dataset.csv')
 olist_products = pd.read_csv(path+'olist_products_dataset.csv')
 olist_sellers = pd.read_csv(path+'olist_sellers_dataset.csv')
 
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -8,3 +8,10 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 from sklearn.base import BaseEstimator, TransformerMixin
 
+path = "data/"
+olist_order_reviews = pd.read_csv(path+'olist_order_reviews_dataset.csv')
+
+df_reviews = olist_order_reviews.loc[:,['review_score','review_comment_message']]
+df_comments = df_reviews.dropna(subset=['review_comment_message'])
+df_comments = df_comments.reset_index(drop = True)
+df_comments.columns = ['score','comment']
