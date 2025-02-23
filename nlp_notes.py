74575bb371b34dda54a70c41ef911cbdc2dc4b80
FRANKSADWAR <billyfranks98@gmail.com>
2025-02-23 20:31:24 +0300

    More NLP Learning

---
+++ nlp_notes.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import re
+
+"""
+Before we start tokenization process, we must clean the data and remove special characters, quotations and punctuation marks from
+text.
+"""
