73b91c1f4b00a9989d91a641053ab238ade609b3
FRANKSADWAR <billyfranks98@gmail.com>
2025-02-23 20:33:30 +0300

    split the word by the occurence of the pattern

--- nlp_notes.py
+++ nlp_notes.py
@@ -6,3 +6,6 @@ import re
 Before we start tokenization process, we must clean the data and remove special characters, quotations and punctuation marks from
 text.
 """
+sentence = """ Thomas Jeferson began building Montivello at the\n age of 26."""
+tokens = re.split(r'[-\s.,:!?]+', sentence)
+tokens
