f9118834bf8b0ff33307d24433618e53fdfae2b4
FRANKSADWAR <billyfranks98@gmail.com>
2025-03-07 17:53:53 +0300

    concatenate dataframes fix

--- ml_utils.py
+++ ml_utils.py
@@ -260,8 +260,11 @@ class BinaryClassifiersAnalysis:
             self.classifiers_info[model_name]['test_performance'] = test_performance
 
             # Building and unique DataFrame with performances retrieved
-            model_performance = train_performance.concat(test_performance, ignore_index = True)
-            df_performances = df_performances.concat(model_performance,  ignore_index = True)
+            model_performance = pd.concat([train_performance, test_performance],ignore_index=True)
+            #model_performance = train_performance.concat(test_performance, ignore_index = True)
+            
+            df_performances = pd.concat([model_performance, model_performance], ignore_index = True)
+            #df_performances = df_performances.concat(model_performance,  ignore_index = True)
 
             # Saving some attributes on model_info dictionary for further access
             model_data = {
