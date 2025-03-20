4f3975e936958c6e1141960cd8a634d4bbb88b64
FRANKSADWAR <billyfranks98@gmail.com>
2025-03-20 09:16:49 +0300

    get the data from the web

---
+++ downloader.py
@@ -0,0 +1,13 @@
+import requests
+from pathlib import Path
+
+def download_one_file_of_raw_data(year: int, month: int) -> Path:
+    URL = f"https://d37ci6vzurychx.cloudfromt.net/trip-data/yellow_trip_data-{year}-{month:02d}.parquet"
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        path = f'.../data/raw_rides_{year}-{month:02d}.parquet'
+        open(path, "wb").write(response.content)
+        return path
+    else:
+        raise Exception(f'{URL} is not available')
